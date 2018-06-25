fefcc677278aabe413ff3d83bf19c04a7a3672e6
Iris Lee <lee.iris.s@gmail.com>
2018-06-25 13:44:26 -0700

    combined  uscis scrape

--- uscis.py
+++ uscis.py
@@ -1,24 +1,41 @@
-import requests,re, os
+import requests,re, os, time
 import pandas as pd
 from bs4 import BeautifulSoup
 
-input_dir = os.path.join(os.getcwd(),'uscis')
+input_dir = os.path.join(os.getcwd(),'uscis','input')
 os.path.exists(input_dir) or os.mkdir(input_dir)
 
-f = open('uscis/links.txt', 'w') 
 
-url = requests.get('https://www.uscis.gov/tools/reports-studies/immigration-forms-data/data-set-form-i-485-application-adjustment-status')
-html = url.content
-soup = BeautifulSoup(html,'html.parser')
+def get_links():
+	url = requests.get('https://www.uscis.gov/tools/reports-studies/immigration-forms-data/data-set-form-i-485-application-adjustment-status')
+	html = url.content
+	soup = BeautifulSoup(html,'html.parser')
+	atags = soup.find_all('a',{'type':re.compile('^text.*')})
+	link_list=[]
 
-# body = soup.find_all('div', {'id':'main-body'})
+	for a in atags:
+		prefix = 'https://uscis.gov'
+		link = prefix+a.get('href')
+		link_list.append(link)
+	print len(link_list)
+	return link_list
 
-atags = soup.find_all('a',{'type':re.compile('^text.*')})
 
+def download_links():
+	link_list = get_links()
+	for link in link_list:
+		time.sleep(5)
+		try:
+			name = re.search(r'(?<=[Pp]erformancedata.)(.*$)',link).group()
+		except AttributeError:
+			name = re.search(r'.{14}$',link).group()
 
-for a in atags:
-	prefix = 'https://uscis.gov'
-	link = prefix+a.get('href')
-	f.write(link +"\n")
+		loc = os.path.join(input_dir, name)
+		response = requests.get(link)
+		with open(loc,'wb') as f:
+			f.write(response.content)
 
-f.close()
+		print ('downloading {}'.format(link))
+
+download_links()
+
